721eee4b4fb551f57e32083ea17499468d400c3e
fernando <nanocatdemen@gmail.com>
2015-10-05 21:53:50 -0300

    Fix conflict on client.sh

--- client.sh
+++ client.sh
@@ -9,8 +9,4 @@ mv DistributedClimbers-Server/src/cl/uchile/dcc/cc5303/*.class DistributedClimbe
 # Run client
 echo "Starting client..."
 cd DistributedClimbers-Client/bin
-<<<<<<< HEAD
-java -Djava.rmi.server.hostname=172.17.69.201 cl.uchile.dcc.cc5303.Main
-=======
 java -Djava.rmi.server.hostname=$1 cl.uchile.dcc.cc5303.Main
->>>>>>> 30a32dc35ab4d81786a45387dec7850d2b4bcecb
