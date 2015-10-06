b73db2746c03ec36ca25d4f54758036c0e80b743
fernando <nanocatdemen@gmail.com>
2015-10-05 22:49:54 -0300

    Update server shell script to detect your ip

--- server.sh
+++ server.sh
@@ -1,22 +1,21 @@
 if [ -z "$1" ]
-  then echo "Please provide an IP to serve."
+  then echo "Please provide the number of players."
   exit
 fi
-if [ -z "$2" ]
-  then echo "Please provide an the number of lives."
-  exit
-fi
+# Get the local IP
+SERVER_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 # Compile files
 echo "Compiling..."
 javac DistributedClimbers-Server/src/cl/uchile/dcc/cc5303/*.java
 # Move to bin folder to keep order
+mkdir -p DistributedClimbers-Server/bin/cl/uchile/dcc/cc5303/
 mv DistributedClimbers-Server/src/cl/uchile/dcc/cc5303/*.class DistributedClimbers-Server/bin/cl/uchile/dcc/cc5303/
 # Restart rmiregistry
 echo "Releasing port..."
-cd DistributedClimbers-Server/bin
 fuser -k 1099/tcp
 echo "Starting rmiregistry..."
+cd DistributedClimbers-Server/bin
 rmiregistry &
 # Run server
 echo "Starting server..."
-java -Djava.rmi.server.hostname=$1 cl.uchile.dcc.cc5303.Server $1 $2
+java -Djava.rmi.server.hostname=$SERVER_IP cl.uchile.dcc.cc5303.Server $SERVER_IP $1
